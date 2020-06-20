dc9981e332554492c80814d75ba6b3c90b7e735f
gulshan-blocumen <gulshan@blocumen.com>
2020-06-20 15:27:46 +0530

    Added login and signup

---
+++ js/login.js
@@ -0,0 +1,7 @@
+function login() {
+  const email = document.getElementsByName('email')[0].value;
+  const password = document.getElementsByName('password')[0].value;
+  // todo: call the api
+}
+
+window.login = login;
