e147f0f4d45108ee59d68094e2689d547371c7b8
gulshanvasnani <gulshan@ost.com>
2020-06-27 13:14:19 +0530

    populated posts on dom

--- js/post.js
+++ js/post.js
@@ -7,75 +7,76 @@
  */
 
 
- function getPosts() {
+function getPosts() {
   // const xhttp = new XMLHttpRequest();
 
   // xhttp.open("GET", "http://localhost:5754/api/v1/getAllPosts", false);
   // xhttp.send();
   let posts;
-  const loginToken = localStorage.getItem('loginToken');  
+  const loginToken = localStorage.getItem('loginToken');
   $.ajax({
     "type": "GET",
     "url": "http://localhost:5754/api/v1/getAllPosts",
-    "headers": {"Authorization": "Bearer "+loginToken},
+    "headers": { "Authorization": "Bearer " + loginToken },
     "dataType": "json",
     success: function (dataString) {
       console.log('**get all posts response from server : ', dataString);
       posts = dataString.posts;
-      console.log('posts : ',posts);
+      console.log('posts : ', posts);
+      populatePostOnDom(posts);
     },
     error: function (failureResponse) {
       console.log('**failure response : ', failureResponse);
     }
   });
   console.log('completed get all postss');
-  // const books = JSON.parse(xhttp.responseText);
 
+}
+
+function populatePostOnDom(posts) {
+  let postCollection;
+  const userId = localStorage.getItem('id');
+  console.log('userId : ',userId);
   for (let post of posts) {
-      const x = `
-          <div class="col-4">
-              <div class="card">
-                  <div class="card-body">
-                      <h5 class="card-title">${post.postContent}</h5>
-                      <h6 class="card-subtitle mb-2 text-muted">${post.result}</h6>
-                      <div>Result: ${post.result}</div>
-                      <hr>
-                      <button type="button" class="btn btn-danger">Delete</button>
-                      <button types="button" class="btn btn-primary" data-toggle="modal" 
-                          data-target="#editBookModal" onClick="setEditModal(${book.isbn})">
-                          Edit
-                      </button>
-                  </div>
-              </div>
-          </div>
-      `
+    postCollection += '<div class="col-12">';
+    postCollection += '<div class="card">';
+    postCollection += '<div class="card-body">';
+    postCollection += `<h5 class="card-title">${post.postContent}</h5>`;
+    postCollection += `<h6 class="card-subtitle mb-2 text-muted">${post.result}</h6>`;
+    postCollection += '<hr>';
+    if(userId !== post._id) {
+    postCollection += `<button types="button" class="btn btn-primary" onClick="postLike('${post._id}')"> Like </button>`;
+  }
+    postCollection += '</div> </div> </div>';
+    // set to localstorage 
 
-      document.getElementById('books').innerHTML = document.getElementById('books').innerHTML + x;
-  
-      // set to localstorage 
-  
-    }
- }
+  }
+  document.getElementById('allposts').innerHTML = document.getElementById('allposts').innerHTML + postCollection;
+}
 
- function submitPost() {
+function postLike(postId) {
+  console.log('postId : ', postId);
+  alert(postId);
+}
 
-  const loginToken = localStorage.getItem('loginToken');  
-  console.log('loginToken : ',loginToken);
+function submitPost() {
+
+  const loginToken = localStorage.getItem('loginToken');
+  console.log('loginToken : ', loginToken);
   const post = document.getElementsByName('submitpost')[0].value;
-  console.log('post : ',post);
+  console.log('post : ', post);
   $.ajax({
     "type": "POST",
     "url": "http://localhost:5754/api/v1/createPost",
     "data": JSON.stringify({
       "postContent": post
     }),
-    "headers": {"Authorization": "Bearer "+loginToken},
+    "headers": { "Authorization": "Bearer " + loginToken },
     "contentType": "application/json",
     "dataType": "json",
     success: function (dataString) {
       console.log('**response from server : ', dataString);
       getPosts();
-      // window.location.reload();
     }
   });
- }
+}
